1012583dc589863f58e6fb179db685d48705ee95
Bijay-Codes <bijayprojects@gmail.com>
2026-07-18 17:55:08 +0530

    set up the initial animation of header using framer motion

--- src/components/hero-sect.tsx
+++ src/components/hero-sect.tsx
@@ -1,11 +1,15 @@
 import type { data_to_show } from "../portfolio-data/data-types";
-
+import { motion } from "framer-motion";
 export function Hero({ data }: { data: data_to_show }) {
     const { aboutMe } = data;
+    const duration = 0.4;
     return (
         <>
-            <Initialize />
-            <header
+            <Initialize duration={duration} />
+            <motion.header
+                initial={{ opacity: 0, scale: 0.8, y: 40 }}
+                animate={{ opacity: 1, scale: 1, y: 0 }}
+                transition={{ delay: duration * 1.5, duration: 0.3, ease: 'easeIn' }}
                 className="flex flex-col sm:grid grid-cols-[1fr_auto] items-end
             bg-surface border-surface-fg relative gap-6
             py-10 px-6 rounded-lg w-full max-w-350">
@@ -17,29 +21,90 @@ export function Hero({ data }: { data: data_to_show }) {
                     <CircleBadges />
                     <Links />
                 </div>
-            </header>
+            </motion.header>
         </>
     )
 }
 
-function Initialize() {
+function Initialize({ duration }: { duration: number }) {
     return (
-        <div className="flex flex-col self-start text-xs text-surface-muted-fg">
-            <span>
-                <span className="text-success">bijay@codes:</span>
-                ~$ sudo init --portfolio</span>
-            <span>bash: ...</span>
-            <span>bash: ...nope, do it yourself</span>
-            <span>
-                <span className="text-success">bijay@codes:</span>
-                ~$ npm run build
-            </span>
-            <span>
-                <span className="text-success">bijay@codes:</span>
-                ~$ npm run preview
-            </span>
-            <span className="h-3 w-1 bg-success animate-pulse duration-75"></span>
-        </div>
+        <motion.div
+            initial={{ inset: '0 10% 0 0' }}
+            animate={{ inset: '0 100% 0 0' }}
+            transition={{ duration: duration * 1.5, ease: 'linear' }}
+            className="flex flex-col self-start text-xs text-surface-muted-fg">
+            <motion.div
+                initial={{ clipPath: "inset(0 100% 0 0)" }}
+                animate={{ clipPath: "inset(0 0% 0 0)" }}
+                transition={{
+                    duration: duration,
+                    ease: "linear"
+                }}
+            >
+                <span className="text-success">bijay@codes:</span> ~$ sudo init --portfolio
+            </motion.div>
+            <motion.div
+                initial={{ clipPath: 'inset(0 100% 0 0)' }}
+                animate={{ clipPath: 'inset(0 0% 0 0)' }}
+                transition={{
+                    duration: duration,
+                    ease: 'linear',
+                    delay: duration * 0.4
+                }}
+            >
+                <span>bash: ...</span>
+            </motion.div>
+            <motion.div
+                initial={{ clipPath: 'inset(0 100% 0 0)' }}
+                animate={{ clipPath: 'inset(0 0% 0 0)' }}
+                transition={{
+                    duration: duration,
+                    ease: 'linear',
+                    delay: duration * 0.6
+                }}
+            >
+                <span>bash: ...nope, do it yourself</span>
+            </motion.div>
+            <motion.div
+                initial={{ clipPath: 'inset(0 100% 0 0)' }}
+                animate={{ clipPath: 'inset(0 0% 0 0)' }}
+                transition={{
+                    duration: duration,
+                    ease: 'linear',
+                    delay: duration * 0.8
+                }}
+            >
+                <span>
+                    <span className="text-success">bijay@codes:</span>
+                    ~$ npm run build
+                </span>
+            </motion.div>
+            <motion.div
+                initial={{ clipPath: 'inset(0 100% 0 0)' }}
+                animate={{ clipPath: 'inset(0 0% 0 0)' }}
+                transition={{
+                    duration: duration,
+                    ease: 'linear',
+                    delay: duration * 1.4
+                }}
+            >
+                <span>
+                    <span className="text-success">bijay@codes:</span>
+                    ~$ npm run preview
+                </span>
+            </motion.div>
+            <motion.span
+                animate={{ opacity: [0, 1] }}
+                transition={{
+                    duration: 0.1,
+                    repeat: Infinity,
+                    repeatType: "reverse",
+                    delay: duration * 1.5,
+                    repeatDelay: 0.4,
+                }}
+                className="h-3 w-1 bg-success"
+            />
+        </motion.div >
     )
 }
 
@@ -95,10 +160,10 @@ function Links() {
 function CircleBadges() {
     const circle_class = 'w-3 h-3 sm:w-4 sm:h-4 rounded-full';
     return (
-        <div className="flex gap-2 lg:gap-3 opacity-40 absolute top-0 right-0 p-1 lg:p-2">
-            <div className={`bg-success ${circle_class}`}></div>
-            <div className={`bg-warning ${circle_class}`}></div>
-            <div className={`bg-danger ${circle_class}`}></div>
+        <div className="flex gap-2 lg:gap-3 absolute top-0 right-0 p-1 lg:p-2">
+            <div className={`bg-success/60 hover:bg-success ${circle_class}`}></div>
+            <div className={`bg-warning/60 hover:bg-warning ${circle_class}`}></div>
+            <div className={`bg-danger/60 hover:bg-danger ${circle_class}`}></div>
         </div>
     )
 }
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -24,33 +24,35 @@ export function Projects({ data }: { data: data_to_show }) {
 function Builds({ work }: { work: project_data }) {
     return (
         // Parent
-        <section key={work.name} className="bg-surface-muted p-4 rounded
-            first:col-span-2
-            flex flex-col gap-4 justify-center">
+        <section key={work.name} className="bg-surface-muted first:col-span-2 rounded-2xl">
             {/* Child- renders project name and terminal badges */}
-            <div className="flex flex-1">
-                <h4 className="text-xl text-surface-muted-fg">
-                    $sudo --pitch {work.name}
-                </h4>
-                {/* three circles in each projects come from this */}
-                <TerminalCircles />
-            </div>
-            {/* details of projects- to edit go to portfolio-data/about-me file */}
-            <p className="text-md max-w-250">bash: {work.intro}</p>
+            <TerminalCircles />
+            <div className="p-4
+            flex flex-col gap-4 justify-center">
+                <div className="flex flex-1">
+                    <h4 className="text-xl text-surface-muted-fg">
+                        $sudo --pitch {work.name}
+                    </h4>
+                    {/* three circles in each projects come from this */}
 
-            {/* time taken to build project */}
-            <span className="text-surface-muted-fg">
-                --time-taken: <span className="text-surface-fg">
-                    {work.completedIn} days</span>
-            </span>
-            {/* tech stack */}
-            <KeyFeatures features={work.keyPoints} />
-            <TechUsed work={work} />
-            {/* Live demo and github link comes from this */}
-            <Links work={work} />
-            {work.name === 'Favdex' &&
-                <Link to='/casestudy' className="text-lg bg-surface py-1 px-4 rounded-lg border border-accent/60 hover:border-accent w-fit font-bold uppercase">Case study</Link>
-            }
+                </div>
+                {/* details of projects- to edit go to portfolio-data/about-me file */}
+                <p className="text-md max-w-250">bash: {work.intro}</p>
+
+                {/* time taken to build project */}
+                <span className="text-surface-muted-fg">
+                    --time-taken: <span className="text-surface-fg">
+                        {work.completedIn} days</span>
+                </span>
+                {/* tech stack */}
+                <KeyFeatures features={work.keyPoints} />
+                <TechUsed work={work} />
+                {/* Live demo and github link comes from this */}
+                <Links work={work} />
+                {work.name === 'Favdex' &&
+                    <Link to='/casestudy' className="text-lg bg-surface py-1 px-4 rounded-lg border border-accent/60 hover:border-accent w-fit font-bold uppercase">Case study</Link>
+                }
+            </div>
         </section>
 
     )
@@ -78,7 +80,7 @@ function TechUsed({ work }: { work: project_data }) {
 export function TerminalCircles() {
     const three_dots = 'w-4 h-4 rounded-full';
     return (
-        <div className="ml-auto flex gap-2 opacity-70 justify-end">
+        <div className="ml-auto flex gap-2 opacity-70 justify-end h-8 items-center px-4 bg-secondary w-full">
             <span className={`${three_dots} bg-success`}></span>
             <span className={`${three_dots} bg-warning`}></span>
             <span className={`${three_dots} bg-danger`}></span>
@@ -94,7 +96,7 @@ function Links({ work }: { work: project_data }) {
                 target="_blank">Live demo</a>
             <a className="py-1 px-4 bg-primary text-primary-fg rounded-lg"
                 href={work.links.sourceLink}
-                target="_blank">Source Code</a>
+                target="_blank"> Source Code</a>
         </div>
     )
 }
